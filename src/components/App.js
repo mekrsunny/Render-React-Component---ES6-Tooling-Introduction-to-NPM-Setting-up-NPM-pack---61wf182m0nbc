a9dfb4d008ffca208bd9137995b6f3fa53533a6c
kumar sunny <coolsunny2099@gmail.com>
2022-11-25 11:16:10 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">Anime Watching Platform</h1>
+      <p> I am learning React. My life is getting better.</p>
+    </div>
   )
 }
 
